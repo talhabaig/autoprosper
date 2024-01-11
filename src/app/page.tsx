8dbf56f5dba0cfe515709009b151d3d82e131023
baigtalha7 <baigtalha7@gmail.com>
2024-01-11 19:59:57 +0500

    discount and first time bayer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import AutoProsperAdvnatage from "@/components/home/AutoProsperAdvnatage";
-import ConsultancyAndGrouth from "@/components/home/ConsultancyAndGrouth";
-import DiscountCreditHistory from "@/components/home/DiscountCreditHistory";
-import FindandMake from "@/components/home/FindandMake";
-import FirstTimeBuyer from "@/components/home/FirstTimeBuyer";
-import GuideAndTips from "@/components/home/GuideAndTips";
-import UpgradeRide from "@/components/home/UpgradeRide";
-import Image from "next/image";
+import AutoProsperAdvnatage from "@/components/home/AutoProsperAdvnatage/AutoProsperAdvnatage";
+import ConsultancyAndGrouth from "@/components/home/ConsultancyAndGrouth/ConsultancyAndGrouth";
+import DiscountCreditHistory from "@/components/home/DiscountCreditHistory/DiscountCreditHistory";
+import FindandMake from "@/components/home/FindandMake/FindandMake";
+import FirstTimeBuyer from "@/components/home/FirstTimeBuyer/FirstTimeBuyer";
+import GuideAndTips from "@/components/home/GuideAndTips/GuideAndTips";
+import UpgradeRide from "@/components/home/UpgradeRide/UpgradeRide";
+
 
 export default function Home() {
   return (
--- src/components/home/FirstTimeBuyer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FirstTimeBuyer = () => {
-  return (
-    <section className='py-6'>FirstTimeBuye Section</section>
-  )
-}
-
-export default FirstTimeBuyer
