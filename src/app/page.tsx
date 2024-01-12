98412ecad025d065805abddc3c1af5b944b54fef
Nova Al Farisi <nova.alfarisi@jubelio.com>
2024-01-12 16:23:37 +0700

    move to directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import AutoProsperAdvnatage from "@/components/home/AutoProsperAdvnatage/AutoProsperAdvnatage";
 import ConsultancyAndGrouth from "@/components/home/ConsultancyAndGrouth/ConsultancyAndGrouth";
 import DiscountCreditHistory from "@/components/home/DiscountCreditHistory/DiscountCreditHistory";
-import Discover from "@/components/home/Discover";
+import Discover from "@/components/home/Discover/Discover";
 import FindandMake from "@/components/home/FindandMake/FindandMake";
 import FirstTimeBuyer from "@/components/home/FirstTimeBuyer/FirstTimeBuyer";
 import GuideAndTips from "@/components/home/GuideAndTips/GuideAndTips";
-import Hero from "@/components/home/Hero";
+import Hero from "@/components/home/Hero/Hero";
 import UpgradeRide from "@/components/home/UpgradeRide/UpgradeRide";
 
 
