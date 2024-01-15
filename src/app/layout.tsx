de5ecc3ef8714dfbbf72fb38095a8d67f80934e4
Nova Al Farisi <nova.alfarisi@jubelio.com>
2024-01-15 22:09:36 +0700

    add floating button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 // import { Inter } from "next/font/google";
 import "./globals.css";
 import "../assests/style/main.css";
+import { MessageSquare } from "lucide-react";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -11,11 +12,11 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1,
   maximumScale: 1,
   userScalable: true,
-}
+};
 
 export default function RootLayout({
   children,
@@ -28,6 +29,11 @@ export default function RootLayout({
         {/* Navbar */}
         {children}
         {/* footer */}
+        <div className="fixed bottom-4 right-4">
+          <button className="bg-primary hover:scale-110 transition-all text-white font-bold p-[14px] rounded-full shadow-lg">
+            <MessageSquare fill="#fff" width="20" height="18" />
+          </button>
+        </div>
       </body>
     </html>
   );
