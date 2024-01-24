fdfec9df531595df9f1eb5f00bde146685f57e5c
pankaj <agrawalpankaj490@gmail.com>
2024-01-24 22:51:26 +0530

    fix: resolve conflict

--- src/app/get-pre-qualified/page.tsx
+++ src/app/get-pre-qualified/page.tsx
@@ -2,16 +2,12 @@ import GetPreQualified from "@/components/GetPreQualified/GetPreQualified";
 import NavigationBar from "../../components/ui/Navigations/navigationBar";
 
 import React from "react";
-import Footer from "@/components/Footer/Footer";
+import Footer from "@/components/footer/Footer";
 
 const page = () => {
   return (
     <>
-<<<<<<< HEAD
-      <NavigationBar variant ='dark'/>
-=======
       <NavigationBar variant="dark" />
->>>>>>> 214883e7251e35a2ee4465e2e35efbec9b882b6f
       <GetPreQualified />
       <Footer/>
     </>
