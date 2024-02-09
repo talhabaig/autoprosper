a9b0d39277ab80603b3e76f42fa48a0714033165
Nova Al Farisi <nova.alfarisi@jubelio.com>
2024-02-10 00:45:37 +0700

    fix: input text overflowing

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
+import { SelectArrow } from "../Icons/Icons";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -22,7 +23,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     const randomId = Math.floor(Math.random() * (100 - 0) + 0) + props.label;
 
     return (
-      <div className="flex flex-col h-[52px] md:h-[72px] items-start justify-start gap-2 md:gap-[9px] rounded-xl px-4 py-[8px] md:py-[15px] bg-gray-50 border-[1px] border-dark-4 appearance-none">
+      <div className="flex flex-col h-[52px] md:h-[72px] items-start justify-start gap-2 md:gap-[9px] rounded-xl px-[18px] pt-[8px] pb-[10px] md:pb-[15px] md:pl-[12px] md:pt-[12px] bg-gray-50 border-[1px] border-dark-4 appearance-none">
         <label
           htmlFor={randomId.toString()}
           className="cursor-text text-left text-dark-4 text-[9px] md:text-[10px] md:leading-[12.6px]"
@@ -31,17 +32,17 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         </label>
         <div className="flex items-center text-[14px] md:text-lg">
           {currency}
-        <input
-          type={type}
-          className={cn(
-            "w-full text-[14px] md:text-lg md:leading-[22.68px] text-dark-1 focus:outline-none bg-gray-50 focus:ring-0 peer",
-            className
-          )}
-          id={randomId.toString()}
-          ref={ref}
-          placeholder=" "
-          {...props}
-        />
+          <input
+            type={type}
+            className={cn(
+              "w-full text-[14px] md:text-lg md:leading-[22.68px] text-dark-1 focus:outline-none bg-gray-50 focus:ring-0 peer",
+              className
+            )}
+            id={randomId.toString()}
+            ref={ref}
+            placeholder=" "
+            {...props}
+          />
         </div>
       </div>
     );
@@ -52,24 +53,26 @@ Input.displayName = "Input";
 const Select: React.FC<SelectProps> = ({ options, ...props }) => {
   const randomId = Math.floor(Math.random() * (100 - 0) + 0) + props.label;
   return (
-    <div className="flex flex-col h-[52px] md:h-[72px] items-start gap-[5px] md:gap-[9px] rounded-xl px-[18px] py-[8px] md:py-[15px] bg-gray-50 border-[1px] border-dark-4">
+    <div className="flex flex-col h-[52px] md:h-[72px] items-start gap-[5px] md:gap-[9px] rounded-xl px-[18px] md:pl-[12px] py-[8px] md:py-[12px] bg-gray-50 border-[1px] border-dark-4">
       <label
         htmlFor={randomId.toString()}
         className="cursor-text text-left text-dark-4 text-[9px] md:text-[10px] md:leading-[12.6px]"
       >
         {props.label}
       </label>
-      <select className="-ml-[4px] w-full text-[14px] md:text-lg md:leading-[22.68px] text-dark-1 focus:outline-none bg-gray-50 focus:ring-0">
-        {options?.map((opt) => (
-          <option key={'opt' + randomId.toString()} value={opt.value}>
-            {opt.label}
-          </option>
-        ))}
-      </select>
+      <div className="flex items-center justify-between w-full">
+        <select className="cursor-pointer appearance-none w-full text-[14px] md:text-lg md:leading-[22.68px] text-dark-1 focus:outline-none bg-gray-50 focus:ring-0">
+          {options?.map((opt) => (
+            <option key={"opt" + randomId.toString()} value={opt.value}>
+              {opt.label}
+            </option>
+          ))}
+        </select>
+        <SelectArrow />
+      </div>
     </div>
   );
 };
-
 
 const TextBox = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
