331db639b9cc51c4a5863f8241a20bc6016f3aff
Umair Baig <umairbaig995@gmail.com>
2024-02-10 16:33:59 +0500

    make component design but having issue in it will be resolve later

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -3737,3 +3737,48 @@ export const UserIconNav: React.FC<propType> = ({
     </svg>
   );
 };
+export const flagIcon: React.FC<propType> = ({
+  width,
+  height,
+  fill,
+  className,
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      fill={fill}
+      className={className}
+      viewBox="0 0 20 20"
+    >
+      <path
+        d="M4.16602 17.5002V3.3335H11.666L11.9993 5.00016H16.666V13.3335H10.8327L10.4993 11.6668H5.83268V17.5002H4.16602ZM12.2077 11.6668H14.9993V6.66683H10.6243L10.291 5.00016H5.83268V10.0002H11.8743L12.2077 11.6668Z"
+        fill="#B7BCC3"
+      />
+    </svg>
+  );
+};
+export const pencilIcon: React.FC<propType> = ({
+  width,
+  height,
+  fill,
+  className,
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      fill={fill}
+      className={className}
+      viewBox="0 0 20 20"
+  
+    >
+      <path
+        d="M4.16667 15.8333H5.35417L13.5 7.6875L12.3125 6.5L4.16667 14.6458V15.8333ZM2.5 17.5V13.9583L13.5 2.97917C13.6667 2.82639 13.8507 2.70833 14.0521 2.625C14.2535 2.54167 14.4653 2.5 14.6875 2.5C14.9097 2.5 15.125 2.54167 15.3333 2.625C15.5417 2.70833 15.7222 2.83333 15.875 3L17.0208 4.16667C17.1875 4.31944 17.309 4.5 17.3854 4.70833C17.4618 4.91667 17.5 5.125 17.5 5.33333C17.5 5.55556 17.4618 5.76736 17.3854 5.96875C17.309 6.17014 17.1875 6.35417 17.0208 6.52083L6.04167 17.5H2.5ZM12.8958 7.10417L12.3125 6.5L13.5 7.6875L12.8958 7.10417Z"
+        fill="#B7BCC3"
+      />
+    </svg>
+  );
+};
