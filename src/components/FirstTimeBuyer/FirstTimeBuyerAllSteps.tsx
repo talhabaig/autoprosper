dd5d0401bb7a683ee924dc0e3bbca5f8f18fe6fe
baigtalha7 <baigtalha7@gmail.com>
2024-02-14 13:39:01 +0500

    immeditae pr for deployment

--- src/components/FirstTimeBuyer/FirstTimeBuyerAllSteps.tsx
+++ src/components/FirstTimeBuyer/FirstTimeBuyerAllSteps.tsx
@@ -19,13 +19,14 @@ import SuggestionForYou from "./SuggestionForYou/SuggestionForYou";
 import TotalSeatRequirements from "./TotalSeatRequirements/TotalSeatRequirements";
 import { showToast } from "../../../toastifyConfig";
 import { usePathname, useSearchParams } from "next/navigation";
+import GetPreQualified from "../GetPreQualified/GetPreQualifiedAllSteps/GetPreQualified";
 const FirstTimeBuyerAllSteps: React.FC<FirstTimeBuyer> = () => {
   const pathname = usePathname();
   const [step, setStep] = useState(1);
   const { state } = useFirstBuyer();
 
   const handleNext = async () => {
-    if (step < 7) {
+    if (step < 8) {
       // if ((step === 2 && state?.Preferred_car_brand?.length === 0) || (step === 2 && state?.PurposeOfUse?.length === 0)) {
       //   showToast("warning", "At least one brand or purpose of use needs to be selected");
       //   return;
@@ -40,110 +41,123 @@ const FirstTimeBuyerAllSteps: React.FC<FirstTimeBuyer> = () => {
     <>
       <NavigationBar variant="dark" />
       {pathname === "/find-or-make" ? (
-        <section className={`mt-auto mainWrapperFirstTimeBuyer `}>
-          <div className="container firstTimeBuyerContainerWrapper">
-            {step > 1 && step < 8 && (
-              <div className=" py-[10px] mb-[28px]">
-                <span
-                  className="inline-block"
-                  onClick={() => setStep(step - 1)}
-                >
-                  <BackArrow className="w-[24px] h-[24px] cursor-pointer" />{" "}
-                </span>
-              </div>
-            )}
+        <>
+          {step < 7 && (
+            <section className={`mt-auto mainWrapperFirstTimeBuyer `}>
+              <div className="container firstTimeBuyerContainerWrapper">
+                {step > 1 && step < 6 && (
+                  <div className=" py-[10px] mb-[28px]">
+                    <span
+                      className="inline-block"
+                      onClick={() => setStep(step - 1)}
+                    >
+                      <BackArrow className="w-[24px] h-[24px] cursor-pointer" />{" "}
+                    </span>
+                  </div>
+                )}
 
-            {step === 1 && <WhatEssential />}
-            {step === 2 && <PurposeOfUse />}
-            {step === 3 && <PreferredCarBrands />}
-            {step === 4 && <TotalSeatRequirements />}
-            {step === 5 && <VehiclePreferences />}
-            {step === 6 && <RidePowerConsumption />}
-          </div>
-          <div className="border-t border-solid border-dark-6 mt-[30px lg:mt-0]">
-            <div className="container flex justify-center lg:justify-end items-center py-[12px] lg:py-[30px]">
-              {step === 6 ? (
-                <Link href="get-pre-qualified">
-                  <Button
-                    variant="small"
-                    className="lg:min-h-[56px]"
-                    onClick={() => {
-                      handleNext();
-                    }}
-                    children={
-                      <>
-                        <span className="lg:hidden">Next</span>
-                        <span className="hidden lg:inline-block">Continue</span>
-                        <ArrowRight className="fill-primary-text w-[1.125rem] h-[1.125rem] lg:w-[1.25rem] lg:h-[1.25rem]" />
-                      </>
-                    }
-                  />
-                </Link>
-              ) : (
-                <Button
-                  variant="small"
-                  className="lg:min-h-[56px]"
-                  onClick={() => {
-                    handleNext();
-                  }}
-                  children={
-                    <>
-                      <span className="lg:hidden">Next</span>
-                      <span className="hidden lg:inline-block">Continue</span>
-                      <ArrowRight className="fill-primary-text w-[1.125rem] h-[1.125rem] lg:w-[1.25rem] lg:h-[1.25rem]" />
-                    </>
-                  }
-                />
-              )}
-            </div>
-          </div>
-        </section>
-      ) : (
-        <section
-          className={`mt-auto mainWrapperFirstTimeBuyer ${
-            step === 6 && "bg-dark-7"
-          }`}
-        >
-          <div className="container firstTimeBuyerContainerWrapper">
-            {step > 1 && step < 6 && (
-              <div className=" py-[10px] mb-[28px]">
-                <span
-                  className="inline-block"
-                  onClick={() => setStep(step - 1)}
-                >
-                  <BackArrow className="w-[24px] h-[24px] cursor-pointer" />{" "}
-                </span>
+                {step === 1 && <WhatEssential />}
+                {step === 2 && <PurposeOfUse />}
+                {step === 3 && <PreferredCarBrands />}
+                {step === 4 && <TotalSeatRequirements />}
+                {step === 5 && <VehiclePreferences />}
+                {step === 6 && <RidePowerConsumption />}
               </div>
-            )}
-
-            {step === 1 && <WhatEssential />}
-            {step === 2 && <PreferredCarBrands />}
-            {step === 3 && <TotalSeatRequirements />}
-            {step === 4 && <VehiclePreferences />}
-            {step === 5 && <RidePowerConsumption />}
-            {step === 6 && <SuggestionForYou />}
-          </div>
-          {step < 6 && (
-            <div className="border-t border-solid border-dark-6 mt-[30px lg:mt-0]">
-              <div className="container flex justify-center lg:justify-end items-center py-[12px] lg:py-[30px]">
-                <Button
-                  variant="small"
-                  className="lg:min-h-[56px]"
-                  onClick={() => {
-                    handleNext();
-                  }}
-                  children={
-                    <>
-                      <span className="lg:hidden">Next</span>
-                      <span className="hidden lg:inline-block">Continue</span>
-                      <ArrowRight className="fill-primary-text w-[1.125rem] h-[1.125rem] lg:w-[1.25rem] lg:h-[1.25rem]" />
-                    </>
-                  }
-                />
+              <div className="border-t border-solid border-dark-6 mt-[30px lg:mt-0]">
+                <div className="container flex justify-center lg:justify-end items-center py-[12px] lg:py-[30px]">
+                  {step === 6 ? (
+                    // <Link href="get-pre-qualified">
+                    <Button
+                      variant="small"
+                      className="lg:min-h-[56px]"
+                      onClick={() => {
+                        handleNext();
+                      }}
+                      children={
+                        <>
+                          <span className="lg:hidden">Next</span>
+                          <span className="hidden lg:inline-block">
+                            Continue
+                          </span>
+                          <ArrowRight className="fill-primary-text w-[1.125rem] h-[1.125rem] lg:w-[1.25rem] lg:h-[1.25rem]" />
+                        </>
+                      }
+                    />
+                  ) : (
+                    // </Link>
+                    <Button
+                      variant="small"
+                      className="lg:min-h-[56px]"
+                      onClick={() => {
+                        handleNext();
+                      }}
+                      children={
+                        <>
+                          <span className="lg:hidden">Next</span>
+                          <span className="hidden lg:inline-block">
+                            Continue
+                          </span>
+                          <ArrowRight className="fill-primary-text w-[1.125rem] h-[1.125rem] lg:w-[1.25rem] lg:h-[1.25rem]" />
+                        </>
+                      }
+                    />
+                  )}
+                </div>
               </div>
-            </div>
+            </section>
           )}
-        </section>
+          {step === 7 && <GetPreQualified onClickProp={handleNext} currentStep={step} />}
+          {step === 8 && <SuggestionForYou />}
+        </>
+      ) : (
+        <>
+          {step < 6 && (
+            <section className={`mt-auto mainWrapperFirstTimeBuyer `}>
+              <div className="container firstTimeBuyerContainerWrapper">
+                {step > 1 && step < 6 && (
+                  <div className=" py-[10px] mb-[28px]">
+                    <span
+                      className="inline-block"
+                      onClick={() => setStep(step - 1)}
+                    >
+                      <BackArrow className="w-[24px] h-[24px] cursor-pointer" />{" "}
+                    </span>
+                  </div>
+                )}
+
+                {step === 1 && <WhatEssential />}
+                {step === 2 && <PreferredCarBrands />}
+                {step === 3 && <TotalSeatRequirements />}
+                {step === 4 && <VehiclePreferences />}
+                {step === 5 && <RidePowerConsumption />}
+              </div>
+              {step < 6 && (
+                <div className="border-t border-solid border-dark-6 mt-[30px lg:mt-0]">
+                  <div className="container flex justify-center lg:justify-end items-center py-[12px] lg:py-[30px]">
+                    <Button
+                      variant="small"
+                      className="lg:min-h-[56px]"
+                      onClick={() => {
+                        handleNext();
+                      }}
+                      children={
+                        <>
+                          <span className="lg:hidden">Next</span>
+                          <span className="hidden lg:inline-block">
+                            Continue
+                          </span>
+                          <ArrowRight className="fill-primary-text w-[1.125rem] h-[1.125rem] lg:w-[1.25rem] lg:h-[1.25rem]" />
+                        </>
+                      }
+                    />
+                  </div>
+                </div>
+              )}
+            </section>
+          )}
+
+          {step === 6 && <SuggestionForYou />}
+        </>
       )}
     </>
   );
